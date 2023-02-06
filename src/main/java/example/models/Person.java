0056cdbaddb35c78df327b099640cb402ff53e62
VeraSlaveholder <bulgakova.vera2003@mail.ru>
2023-02-07 01:19:55 +0300

    .

--- src/main/java/example/models/Person.java
+++ src/main/java/example/models/Person.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.*;
 //import javax.persistence.*;
 
 public class Person {
-    //    @Column
+    //    @Column(name="id")
     private int id;
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 100, message = "Name should be between 2 and 100 characters")
